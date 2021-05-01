4a5587323cf26876b96ea699355eb3fd092bd102
tuxfoo <tuxfoo@librem.one>
2021-05-01 17:54:04 +1200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# simple-donation-ticker for nodecg
+
+This is WIP. While you can use this in production, this bundle does not do any sort of user input validation on the form; an incorrect value could result in the events appearing. It is recommended that you only run one instance of index.html at a time.
+
+Contributions and improvements are welcome.
+
+Display's information about tips/donations. Currently it displays the total amount tipped overall.
+There will be multiple layouts and customisation options.
+
+This bundle is only responsible for creating and rendering a list of tippers. The events will be triggered/activated by other bundles/software/scripts/bots using a rest API.
+
+## How to send an donations using cURL
+
+Make sure to replace the username and amount
+```
+curl -X POST -H "Content-Type:application/json" http://localhost:9090/simple-donation-ticker/ticker -d '{"name":"foobar", "amount": 5 }'
+```
+
+## How to send donations using invoke-webrequest (WINDOWS PowerShell)
+Make sure to replace the username and amount
+```
+invoke-webrequest -Uri http://localhost:9090/simple-donation-ticker/ticke -Method POST -Headers @{'Content-Type' = 'application/json; charset=utf-8'} -Body '{"name":"Foobar", "amount": 5 }' -UseBasicParsing
+```
---
+++ extension/index.js
@@ -0,0 +1,57 @@
+'use strict';
+
+module.exports = function (nodecg) {
+	// A list of tippers totals
+	nodecg.Replicant('tipTotals', { defaultValue: []});
+	nodecg.Replicant('recentTips', { defaultValue: []});
+	nodecg.Replicant('settings', { defaultValue: {minAmount: 1, donorColour: "#9B91FF", textColour: "#32C3A6", currency: "suffix", recent: 15, currencyName: "LBC", mode: "top"}});
+  nodecg.Replicant('manual', { defaultValue: {}, persistent: false });
+	const router = nodecg.Router();
+	var tipTotals = nodecg.Replicant('tipTotals');
+	const recentTips = nodecg.Replicant('recentTips');
+	const settings = nodecg.Replicant('settings');
+	const manual = nodecg.Replicant('manual');
+
+	function addTotal(username, amount) {
+		const sorted = [...tipTotals.value];
+		var tipExists = sorted.map(
+		function(e) {
+			return e.name
+		}).indexOf(username);
+		if (tipExists != -1) {
+				sorted[tipExists].amount = parseFloat(sorted[tipExists].amount) + parseFloat(amount);
+		}
+		else {
+			sorted.push({name: username, amount: amount});
+		}
+		sorted.sort( function (a, b) { return parseFloat(b.amount) - parseFloat(a.amount) } );
+		tipTotals.value = sorted;
+	}
+
+	function addRecent(username, amount) {
+		while ( recentTips.value.length > settings.value.recent ) {
+			recentTips.shift();
+		}
+		if ( amount > settings.value.minAmount ) {
+			recentTips.value.push({name: username, amount: amount});
+		}
+		addTotal(username, amount);
+	}
+
+	manual.on('change', v => {
+		if (v.name && v.amount) {
+			nodecg.log.info('Manual Change');
+			addRecent(v.name, v.amount);
+		}
+	});
+
+	router.post('/ticker', (req, res) => {
+			const name = req.body.name;
+			const amount = req.body.amount;
+			res.send("Donation will be added to ticker if it meets requirements");
+			addRecent(name, amount);
+	});
+
+		nodecg.mount('/simple-donation-ticker', router); // The route '/my-bundle/customroute` is now available
+
+};
